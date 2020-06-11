2366e7d8099c0be504e5ad2b6077afd9990e9063
SandraRawicz <sandra.rawicz@gmail.com>
2020-06-11 17:01:59 +0200

    add player tests

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import sessionmaker
 
 from db.base import Base
 from exceptions import MissingRequiredParameterError
-from models.user import Address, EscapeRoomOwnerPerson, Person
+from models.user import Address, EscapeRoomOwnerPerson, Person, Player
 
 
-class TestAddress(test.TestCase):
+class TestUser(test.TestCase):
     engine = create_engine('sqlite:///test.db')
     Session = sessionmaker(bind=engine)
 
@@ -17,6 +17,8 @@ class TestAddress(test.TestCase):
         Base.metadata.drop_all(self.engine)
         Base.metadata.create_all(self.engine)
 
+
+class TestAddress(test.TestCase):
     def test_address_without_mandatory_field_raises_exception(self):
         parameters = ("city", "postcode", None, 1, 2)
         self.assertRaises(MissingRequiredParameterError, Address, *parameters)
@@ -25,7 +27,9 @@ class TestAddress(test.TestCase):
         address = Address("city", "postcode", "street", 1)
         self.assertIsNotNone(address)
 
-    def test_create_escape_room_owner_person_must_have_person(self):
+
+class Test(TestUser):
+    def test_escape_room_owner_person_without_person_raises_exception(self):
         session = self.Session()
         address = Address("city", "postcode", "street", 1)
         owner = EscapeRoomOwnerPerson(username="us", password="ps", address=address)
@@ -52,4 +56,28 @@ class TestAddress(test.TestCase):
         self.assertEqual(saved_owner.er_owner_person.first_name, "first")
         self.assertEqual(saved_owner.er_owner_person.last_name, "last")
 
+        session.close()
+
+
+class TestPlayer(TestUser):
+    def test_create_player(self):
+        session = self.Session()
+        person = Person(first_name="first", last_name="last")
+        player = Player(username="us", password="ps", person=person)
+        session.add(player)
+        session.commit()
+
+        saved_player = session.query(Player)[0]
+        self.assertEqual(saved_player.username, "us")
+        self.assertEqual(saved_player.password, "ps")
+        self.assertEqual(saved_player.person.first_name, "first")
+        self.assertEqual(saved_player.person.last_name, "last")
+
+        session.close()
+
+    def test_player_without_person_raises_exception(self):
+        session = self.Session()
+        player = Player(username="us", password="ps")
+        session.add(player)
+        self.assertRaises(IntegrityError, session.commit)
         session.close()
