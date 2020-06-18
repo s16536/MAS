df5c81e61c2772f5051a4be99ae759b5b67b8fcb
SandraRawicz <sandra.rawicz@gmail.com>
2020-06-18 22:27:46 +0200

    add user to url

--- src/web/router.py
+++ src/web/router.py
@@ -8,29 +8,32 @@ from web.error_pages import get_error_message
 
 
 def bind(app):
-    app.route('/')(homepage)
-    app.route('/register_visit')(register_visit)
-    app.route('/register_visit/<int:room_id>')(register_visit_in_room)
-    app.route('/register_visit/<int:room_id>/<int:group_id>', methods=['GET', 'POST'])(
+    app.route('/<int:user_id>')(homepage)
+    app.route('/<int:user_id>/register_visit/')(register_visit)
+    app.route('/<int:user_id>/register_visit/<int:room_id>')(register_visit_in_room)
+    app.route('/<int:user_id>/register_visit/<int:room_id>/<int:group_id>', methods=['GET', 'POST'])(
         register_visit_in_room_with_group)
 
 
-def register_visit():
+def register_visit(user_id: int):
+    user = models.User.query.get(user_id)
     escape_rooms = models.EscapeRoom.query.all()
-    return render_template('new_visit.html', rooms=escape_rooms)
+    return render_template('new_visit.html', user=user, rooms=escape_rooms)
 
 
-def register_visit_in_room(room_id):
+def register_visit_in_room(user_id: int, room_id: int):
+    user = models.User.query.get(user_id)
     room = models.EscapeRoom.query.get(room_id)
     groups = models.Group.query.all()
-    return render_template('new_visit_group_selection.html', room=room, groups=groups)
+    return render_template('new_visit_group_selection.html', user=user, room=room, groups=groups)
 
 
-def register_visit_in_room_with_group(room_id, group_id):
+def register_visit_in_room_with_group(user_id: int, room_id: int, group_id: int):
+    user = models.User.query.get(user_id)
     group = models.Group.query.get(group_id)
     room = models.EscapeRoom.query.get(room_id)
     if request.method == 'GET':
-        return render_template('new_visit_other_info.html', room=room, group=group)
+        return render_template('new_visit_other_info.html', user=user, room=room, group=group)
     elif request.method == 'POST':
         print(request.form)
         try:
@@ -49,5 +52,6 @@ def register_visit_in_room_with_group(room_id, group_id):
         return render_template('new_visit_registered.html')
 
 
-def homepage():
-    return render_template('homepage.html')
+def homepage(user_id: id):
+    user = models.User.query.get(user_id)
+    return render_template('homepage.html', user=user)
